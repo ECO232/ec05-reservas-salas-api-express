7571ae17e1cf63603ef27ec104f8e9f80e432b28
Alex <98567345+Axelito02@users.noreply.github.com>
2023-09-03 18:25:24 -0500

    agregando registro de usuarios y de las salas, filtro de usuario por nombre

--- index.js
+++ index.js
@@ -1,25 +1,92 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.use(cors())
-app.use(express.json)
-app.use(express.urlencoded({extended: true}))
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
-let reservas = []
+let users = [];
+let salas = [];
 
-app.post('/reservas',(req, res)=> {
-    const newReservas = {
-        id: req.body.id,
-        name: req.body.name,
-        nameUser: req.nameUser,
-        location: req.body.location,
-        hour: req.body.hour
-    }
-    reservas.push(newReservas)
-    res.send("Tu reserva ha sido registrada")
+users.push({
+    id: "1130613432",
+    name: "Camilo",
+    lastname: "Perez",
 })
 
-app.listen(port, ()=> {
-    console.log(`Servidor corriendo ${port}`);
+users.push({
+    id: "1130613425",
+    name: "Sofia",
+    lastname: "Perez",
+})
+
+// Lista de usuarios
+app.get('/users', (req,res)=>{
+    res.send({"Usuarios registrados": users})
+})
+
+// Registro de usuarios
+app.post('/users',(req, res)=>{
+    const newUser = {
+        id: req.body.id,
+        name: req.body.name,
+        lastname: req.body.lastname,
+    }
+    users.push(newUser)
+    res.send("¡Te has registrado correctamente!")
+})
+
+// Buscar reservas por id del usuario
+app.get('/users/:id', (req, res)=>{
+    const requestID = req.params.id;
+    let requesUser = null;
+    for (let i = 0; i < users.length; i++) {
+        console.log(users[i].id === requestID, users[i].id, requestID);
+        if (users[i].id === requestID) {
+            requesUser = users[i]
+        }
+    }
+    console.log(requesUser, salas);
+    res.json({"usuario": requesUser, "salas reservadas": salas} )
+})
+
+// Filtrar usuarios por el nombre
+app.get('/users', (req, res) =>{
+    console.log(req.query.name);
+    if(req.query.name){
+        users = users.filter((users)=>{
+            return users.name == req.query.name
+        })
+    }
+    res.send({"Usuario": users})
+})
+
+// Eliminar usuario por id
+app.delete('/user/:id', (req,res) =>{
+    const idUser = req.params.id;
+    let deleteUser = users.findIndex(users=> users.id == idUser)
+    let indeUserDelete= users.splice(deleteUser, 1)
+    res.send("El usuario con id"+ indeUserDelete[0].id + "ha sido eliminado")
+})
+
+
+/////////////////////////////////////////////////////////////////
+// Salas disponibles
+app.get('/salas', (req, res)=>{
+    res.send({"Salas disponibles": salas})
+})
+
+// Registrar una reserva
+app.post('/salas', (req, res)=>{
+    const newReserva = {
+        name: req.body.name,
+        location: req.body.location,
+        schedule : req.body.schedule,
+    }
+    salas.push(newReserva)
+    res.send("Tu solicitud ha sido registrada")
+})
+
+app.listen(port ,() => {
+    console.log(`Servidor inicializado en el puerto ${port}`);
 })
